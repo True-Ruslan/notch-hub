e99cf78ea755ea2dff865cc1860c14100b755752
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-08 10:21:09 +0300

    test: pass layout through hosting boundary

--- Tests/NotchHubCoreTests/NotchHostingViewFactoryTests.swift
+++ Tests/NotchHubCoreTests/NotchHostingViewFactoryTests.swift
@@ -1,3 +1,4 @@
+import CoreGraphics
 import Testing
 @testable import NotchHubCore
 
@@ -6,7 +7,13 @@ struct NotchHostingViewFactoryTests {
     @Test
     func hostingViewDoesNotOwnWindowSizing() {
         let model = NotchPanelModel()
-        let hostingView = NotchHostingViewFactory.make(model: model)
+        let layout = NotchLayout(
+            hasHardwareNotch: true,
+            hardwareNotchWidth: 176,
+            compactFrame: CGRect(x: 412, y: 868, width: 176, height: 32),
+            expandedFrame: CGRect(x: 240, y: 650, width: 520, height: 250)
+        )
+        let hostingView = NotchHostingViewFactory.make(model: model, layout: layout)
 
         #expect(hostingView.sizingOptions == [])
     }
