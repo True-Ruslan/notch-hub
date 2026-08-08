f2d8260d9fad7df268930a6e904d3db4305f2d3f
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-08 13:43:35 +0300

    perf: encode notch interaction intent as one enum

--- Sources/NotchHubCore/Notch/NotchInteractionIntent.swift
+++ Sources/NotchHubCore/Notch/NotchInteractionIntent.swift
@@ -1,11 +1,5 @@
-enum NotchInteractionCause: Equatable, Sendable {
-    case deliberateHover
-    case pointerExit
-    case programmatic
-}
-
-struct NotchInteractionIntent: Equatable, Sendable {
-    let desiredPresentation: NotchPresentation
-    let cause: NotchInteractionCause
-    let hapticEligible: Bool
+enum NotchInteractionIntent: Equatable, Sendable {
+    case deliberateExpansion
+    case pointerExitCollapse
+    case programmaticExpansion
 }
