86fd6146790ef9204607a39209cb1cc787c39d49
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-08 13:46:33 +0300

    perf: defer animation policy object to accessibility provider

--- Sources/NotchHubCore/Notch/NotchAnimationPolicy.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct NotchAnimationPolicy: Equatable, Sendable {
-    let duration: TimeInterval
-
-    static let standard = NotchAnimationPolicy(duration: 0.20)
-    static let reducedMotion = NotchAnimationPolicy(duration: 0)
-}
