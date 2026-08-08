6df44c1b00f5a317b2e58d3c6f406d3d53df0a5c
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-08 13:28:32 +0300

    perf: minimize notch animation policy metadata

--- Sources/NotchHubCore/Notch/NotchAnimationPolicy.swift
+++ Sources/NotchHubCore/Notch/NotchAnimationPolicy.swift
@@ -1,25 +1,8 @@
 import Foundation
 
-enum NotchAnimationTiming: Equatable, Sendable {
-    case easeInOut
-}
-
 struct NotchAnimationPolicy: Equatable, Sendable {
     let duration: TimeInterval
-    let timing: NotchAnimationTiming
 
-    static let standard = NotchAnimationPolicy(
-        duration: 0.20,
-        timing: .easeInOut
-    )
-
-    static let reducedMotion = NotchAnimationPolicy(
-        duration: 0,
-        timing: .easeInOut
-    )
-}
-
-@MainActor
-protocol NotchAnimationPolicyProviding: AnyObject {
-    var currentPolicy: NotchAnimationPolicy { get }
+    static let standard = NotchAnimationPolicy(duration: 0.20)
+    static let reducedMotion = NotchAnimationPolicy(duration: 0)
 }
