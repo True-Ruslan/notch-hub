4eabdc0b8b1cb162a3271fa71f6dbdef7ff1ed07
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-09 14:07:24 +0300

    refactor: keep controller command I/O asynchronous

--- Sources/NotchHubMediaCore/MediaSessionController.swift
+++ Sources/NotchHubMediaCore/MediaSessionController.swift
@@ -41,7 +41,7 @@ final class MediaSessionController {
         publish(state: .unavailable, snapshot: nil)
     }
 
-    func send(_ command: MediaCommand) -> MediaCommandResult {
+    func send(_ command: MediaCommand) async -> MediaCommandResult {
         guard isStarted, !isTerminal, let snapshot else {
             return .failed
         }
@@ -67,7 +67,7 @@ final class MediaSessionController {
             }
         }
 
-        return provider.send(command)
+        return await provider.send(command)
     }
 
     private func installHandlerAndStartProvider() {
