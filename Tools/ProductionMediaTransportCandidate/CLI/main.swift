d41d52ff8699eeb14f28ab4f5c05dfcd82984091
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-09 16:51:38 +0300

    fix: resolve production candidate from main bundle

--- Tools/ProductionMediaTransportCandidate/CLI/main.swift
+++ Tools/ProductionMediaTransportCandidate/CLI/main.swift
@@ -13,7 +13,11 @@ struct MediaTransportCandidateMain {
             let invocation = try ProductionMediaTransportCandidateInvocation.parse(
                 arguments: Array(CommandLine.arguments.dropFirst())
             )
-            let paths = try candidatePaths()
+            let paths = try ProductionMediaTransportCandidateBundlePaths.resolve(
+                bundleURL: Bundle.main.bundleURL,
+                resourceURL: Bundle.main.resourceURL,
+                sourceCommit: Bundle.main.object(forInfoDictionaryKey: "NHSourceCommit") as? String
+            )
             let runner = ProductionMediaTransportCandidateRunner(
                 scriptURL: paths.scriptURL,
                 frameworkURL: paths.frameworkURL,
@@ -35,40 +39,6 @@ struct MediaTransportCandidateMain {
             )
             Foundation.exit(EXIT_FAILURE)
         }
-    }
-
-    private static func candidatePaths() throws -> (
-        scriptURL: URL,
-        frameworkURL: URL,
-        sourceCommit: String
-    ) {
-        let executableURL = URL(fileURLWithPath: CommandLine.arguments[0]).standardizedFileURL
-        let contentsURL = executableURL.deletingLastPathComponent().deletingLastPathComponent()
-        let resourcesURL = contentsURL.appendingPathComponent("Resources", isDirectory: true)
-        let infoURL = contentsURL.appendingPathComponent("Info.plist", isDirectory: false)
-
-        guard
-            let infoData = try? Data(contentsOf: infoURL),
-            let propertyList = try? PropertyListSerialization.propertyList(
-                from: infoData,
-                options: [],
-                format: nil
-            ),
-            let info = propertyList as? [String: Any],
-            let sourceCommit = info["NHSourceCommit"] as? String,
-            sourceCommit.range(of: "^[0-9a-f]{40}$", options: .regularExpression) != nil
-        else {
-            throw ProductionMediaTransportCandidateError.invalidArguments
-        }
-
-        return (
-            scriptURL: resourcesURL.appendingPathComponent("mediaremote-adapter.pl", isDirectory: false),
-            frameworkURL: resourcesURL.appendingPathComponent(
-                "MediaRemoteAdapter.framework",
-                isDirectory: true
-            ),
-            sourceCommit: sourceCommit
-        )
     }
 
     private static func writeJSON<T: Encodable>(_ value: T) throws {
