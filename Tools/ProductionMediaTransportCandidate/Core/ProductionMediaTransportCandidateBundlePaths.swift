5aab3c2aa2d81c68bdca2ea4c525d742e65f7a93
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-10 18:03:59 +0300

    style: normalize candidate bundle resolver

--- Tools/ProductionMediaTransportCandidate/Core/ProductionMediaTransportCandidateBundlePaths.swift
+++ Tools/ProductionMediaTransportCandidate/Core/ProductionMediaTransportCandidateBundlePaths.swift
@@ -1,30 +1,41 @@
 import Foundation
-import NotchHubMediaCore
 
 public struct ProductionMediaTransportCandidateBundlePaths: Equatable, Sendable {
     public let scriptURL: URL
     public let frameworkURL: URL
     public let sourceCommit: String
 
-    public static func resolve(bundleURL: URL, resourceURL: URL?, sourceCommit: String?) throws -> Self {
+    public static func resolve(
+        bundleURL: URL,
+        resourceURL: URL?,
+        sourceCommit: String?
+    ) throws -> Self {
         let normalizedBundleURL = bundleURL.standardizedFileURL
-        let expectedResourceURL = normalizedBundleURL
+        let expectedResourceURL =
+            normalizedBundleURL
             .appendingPathComponent("Contents", isDirectory: true)
             .appendingPathComponent("Resources", isDirectory: true)
             .standardizedFileURL
 
-        guard normalizedBundleURL.pathExtension == "app",
-              let resourceURL,
-              resourceURL.standardizedFileURL == expectedResourceURL,
-              let sourceCommit,
-              sourceCommit.range(of: "^[0-9a-f]{40}$", options: .regularExpression) != nil
+        guard
+            normalizedBundleURL.pathExtension == "app",
+            let resourceURL,
+            resourceURL.standardizedFileURL == expectedResourceURL,
+            let sourceCommit,
+            sourceCommit.range(of: "^[0-9a-f]{40}$", options: .regularExpression) != nil
         else {
             throw ProductionMediaTransportCandidateError.invalidArguments
         }
 
         return Self(
-            scriptURL: expectedResourceURL.appendingPathComponent("mediaremote-adapter.pl"),
-            frameworkURL: expectedResourceURL.appendingPathComponent("MediaRemoteAdapter.framework", isDirectory: true),
+            scriptURL: expectedResourceURL.appendingPathComponent(
+                "mediaremote-adapter.pl",
+                isDirectory: false
+            ),
+            frameworkURL: expectedResourceURL.appendingPathComponent(
+                "MediaRemoteAdapter.framework",
+                isDirectory: true
+            ),
             sourceCommit: sourceCommit
         )
     }
