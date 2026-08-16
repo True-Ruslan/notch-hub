a4fb4cd6074567ed5ed5817494d3abd431fccc80
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-16 12:46:16 +0300

    Refactor: make bounded transport cancellation nonblocking

--- Sources/NotchHubMediaCore/MediaRemoteSystemTransport.swift
+++ Sources/NotchHubMediaCore/MediaRemoteSystemTransport.swift
@@ -66,6 +66,21 @@ final class MediaRemoteSystemTransport: SystemMediaTransport {
     }
 
     func stop() {
+        stop(processClientStop: processClient.stop)
+    }
+
+    func stopNonBlocking() {
+        stop(processClientStop: processClient.stopNonBlocking)
+    }
+
+    func send(_ command: MediaCommand) async -> MediaCommandResult {
+        guard isStarted else {
+            return .failed
+        }
+        return await processClient.send(command)
+    }
+
+    private func stop(processClientStop: () -> Void) {
         guard isStarted else {
             return
         }
@@ -74,19 +89,12 @@ final class MediaRemoteSystemTransport: SystemMediaTransport {
         handlerGeneration &+= 1
         processClient.onPayload = nil
         processClient.onFailure = nil
-        processClient.stop()
+        processClientStop()
         activeFingerprint = nil
         activePayload = nil
         activeCapabilities = Self.unknownCapabilities
         publishedNoSession = false
         eventHandler?(.stopped)
-    }
-
-    func send(_ command: MediaCommand) async -> MediaCommandResult {
-        guard isStarted else {
-            return .failed
-        }
-        return await processClient.send(command)
     }
 
     private func receive(_ payload: MediaRemoteWirePayload?) {
