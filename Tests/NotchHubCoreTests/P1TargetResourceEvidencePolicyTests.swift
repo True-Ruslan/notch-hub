8beb8a3220b8d336a32d8ed28b67708f55629f5b
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-19 21:50:50 +0300

    Test P1 platform family in canonical Swift gate

--- Tests/NotchHubCoreTests/P1TargetResourceEvidencePolicyTests.swift
+++ Tests/NotchHubCoreTests/P1TargetResourceEvidencePolicyTests.swift
@@ -20,7 +20,8 @@ struct P1TargetResourceEvidencePolicyTests {
             "-m",
             "unittest",
             "-v",
-            "test_p1_target_resource_evidence.py"
+            "test_p1_target_resource_evidence.py",
+            "test_p1_target_platform_family.py"
         ]
         process.currentDirectoryURL = scriptsDirectory
         process.standardOutput = stdout
